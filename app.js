1805817f894c31e4f381f6f58bcee9c43761b242
petersapountzis <peter.sapountzis@gmail.com>
2023-05-18 18:11:59 -0700

    added weather data to site

--- app.js
+++ app.js
@@ -9,66 +9,80 @@ const outputElement = document.querySelector(".GPT_response");
 let temp, conditions, humidity, wind, city_fromAPI;
 
 async function getWeather(city) {
-  const response = await fetch(
-    `${WEATHER_API_URL}${city}&appid=${WEATHER_API_KEY}&units=imperial`
-  );
-  if (response.status == 404) {
-    document.querySelector(".weather").style.display = "none";
-    document.querySelector(".error").style.display = "block";
-    return;
-  } else {
-    var data = await response.json();
-    console.log(data);
-    conditions = data.weather[0].main;
-    temp = data.main.temp;
-    humidity = data.main.humidity;
-    wind = data.wind.speed;
-    city_fromAPI = data.name;
+	const response = await fetch(
+		`${WEATHER_API_URL}${city}&appid=${WEATHER_API_KEY}&units=imperial`
+	);
+	if (response.status == 404) {
+		document.querySelector(".response").style.display = "none";
+		document.querySelector(".error").style.display = "block";
+		return;
+	} else {
+		var data = await response.json();
+		console.log(data);
+		conditions = data.weather[0].main;
+		temp = data.main.temp;
+		humidity = data.main.humidity;
+		wind = data.wind.speed;
+		city_fromAPI = data.name;
 
-    getGPTData();
-  }
+		getGPTData();
+
+		document.querySelector(".city").innerHTML = data.name;
+		document.querySelector(".temp").innerHTML = data.main.temp + "°f";
+		document.querySelector(".humidity").innerHTML =
+			data.main.humidity + "%";
+		document.querySelector(".wind").innerHTML = data.wind.speed + "mph";
+
+		if (data.weather[0].main == "Clouds") {
+			document.querySelector(".weather-icon").src = "images/cloud.png";
+		} else if (data.weather[0].main == "Rain") {
+			document.querySelector(".weather-icon").src = "images/rain.png";
+		} else if (data.weather[0].main == "Clear") {
+			document.querySelector(".weather-icon").src = "images/clear.png";
+		} else if (data.weather[0].main == "Snow") {
+			document.querySelector(".weather-icon").src = "images/snow.png";
+		} else if (data.weather[0].main == "Drizzle") {
+			document.querySelector(".weather-icon").src = "images/drizzle.png";
+		}
+
+		document.querySelector(".weather").style.display = "block";
+		document.querySelector(".error").style.display = "none";
+	}
 }
 
-
 async function getGPTData() {
-  
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${GPT_API_KEY}`
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: `You are a wardrobe assistant. I am going to give you current weather data and you are going to give me a recommendation on what to wear. It is currently ${temp} degrees Fahrenheit and ${conditions} with ${humidity}% humidity and ${wind}mph wind speeds in ${city_fromAPI}. What should I wear?` }],
-        max_tokens: 100
-      })
-    };
-  
-    try {
-      const response = await fetch(
-        `${GPT_API_URL}`,
-        options
-      );
-      const GPTData = await response.json();
-      outputElement.textContent = GPTData.choices[0].message.content;
-      console.log(temp);
-      console.log(GPTData);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-  
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${GPT_API_KEY}`
+		},
+		body: JSON.stringify({
+			model: "gpt-3.5-turbo",
+			messages: [
+				{
+					role: "user",
+					content: `You are a wardrobe assistant. I am going to give you current weather data and you are going to give me a recommendation on what to wear. 
+        It is currently ${temp} degrees Fahrenheit and ${conditions} with ${humidity}% humidity and ${wind}mph wind speeds in ${city_fromAPI}. Be as specific as possible. Limit to 1-3 sentences. What should I wear?`
+				}
+			],
+			max_tokens: 100,
+			temperature: 1.3
+		})
+	};
+
+	try {
+		const response = await fetch(`${GPT_API_URL}`, options);
+		const GPTData = await response.json();
+		outputElement.textContent = GPTData.choices[0].message.content;
+		console.log(GPTData);
+	} catch (err) {
+		console.error(err);
+	}
+}
 
 searchBtn.addEventListener("click", () => {
-  console.log("clicked");
-  const city = searchBox.value;
-  getWeather(city);
+	console.log("clicked");
+	const city = searchBox.value;
+	getWeather(city);
 });
-
-
-
-  
-  
-  
-  
